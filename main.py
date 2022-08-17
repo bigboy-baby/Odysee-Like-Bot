cccd2d86aa82e14b6f3a69c7b7160c904d71823b
bigboy-baby <63435309+bigboy-baby@users.noreply.github.com>
2022-08-17 18:44:27 -0400

    Added Code & Config

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests, threading, json
+
+with open('config.json') as config_file:
+  config = json.load(config_file)
+
+def like(claim,prox):
+  proxy = {
+    "http":f"http://{prox}",
+    "https":f"http://{prox}",
+  }
+  while True:
+    try:
+      r = requests.get('https://api.odysee.com/user/new', proxies=proxy)
+      dat = r.json()
+      token1 = dat['data']
+      token = token1['auth_token']
+
+      d = {
+        'auth_token': token,
+        'claim_ids': claim,
+        'type': 'like',
+        'clear_types': 'dislike'
+      }
+
+      r = requests.post('https://api.odysee.com/reaction/react', data=d, proxies=proxy)
+    
+      print(f"{token} | Liked {claim}")    
+    except:
+      pass
+
+claim = input("Claim ID: ")
+am = int(input("Thread Amount: "))
+
+prox = config["proxy"]
+for n in range(am):
+  x = threading.Thread(target=like, args=(claim,prox))
+  x.start()
